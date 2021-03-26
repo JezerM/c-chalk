7ff510a2a47e00f09e801cc5280aff057ac70551
Jezer Mejía <59768785+JezerM@users.noreply.github.com>
2021-03-26 15:39:50 -0600

    Changed idiom things

--- main.cpp
+++ main.cpp
@@ -7,16 +7,15 @@ using namespace std;
 chalkClass chalk;
 
 int main() {
-  system("clear");
-  string t = chalk.style("Hola", "bgBrightBlue");
+  string t = chalk.style("Hello", "bgBrightBlue");
   t = chalk.style(t, "red");
   cout << t << endl;
-  string o = chalk.style("Hola", 2, false);
+  string o = chalk.style("Hellouda", 2, false);
   cout << o << endl;
-  cout << chalk.style("Hola a todos", 220, 122, 122) << endl;
-  cout << chalk.style("Hola a todos", 255, 122, 122) << endl;
+  cout << chalk.style("Hello everyone", 220, 122, 122) << endl;
+  cout << chalk.style("Hello everyone", 255, 122, 122) << endl;
   cout << truecolor_to_8bits(220, 122, 122) << endl;
   cout << truecolor_to_8bits(255, 122, 122) << endl;
-  cout << chalk.style("Hola a todos", truecolor_to_8bits(255,122,122), true) << endl;
+  cout << chalk.style("Hello everyone", truecolor_to_8bits(255,122,122), true) << endl;
   return 0;
 }
